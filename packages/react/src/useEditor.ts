a70c3bcc101803c6f9db4cdafb65099fd6938a13
naaa760 <neh6a683@gmail.com>
2026-01-17 01:01:08 +0530

    fix(react): handle undefined options.current in useEditor callbacks

--- packages/react/src/useEditor.ts
+++ packages/react/src/useEditor.ts
@@ -87,8 +87,6 @@ class EditorInstanceManager {
   private setEditor(editor: Editor | null) {
     this.editor = editor
     this.instanceId = Math.random().toString(36).slice(2, 9)
-
-    // Notify all subscribers that the editor instance has been created
     this.subscriptions.forEach(cb => cb())
   }
 
@@ -96,25 +94,18 @@ class EditorInstanceManager {
     if (this.options.current.immediatelyRender === undefined) {
       if (isSSR || isNext) {
         if (isDev) {
-          /**
-           * Throw an error in development, to make sure the developer is aware that tiptap cannot be SSR'd
-           * and that they need to set `immediatelyRender` to `false` to avoid hydration mismatches.
-           */
           throw new Error(
             'Tiptap Error: SSR has been detected, please set `immediatelyRender` explicitly to `false` to avoid hydration mismatches.',
           )
         }
 
-        // Best faith effort in production, run the code in the legacy mode to avoid hydration mismatches and errors in production
         return null
       }
 
-      // Default to immediately rendering when client-side rendering
       return this.createEditor()
     }
 
     if (this.options.current.immediatelyRender && isSSR && isDev) {
-      // Warn in development, to make sure the developer is aware that tiptap cannot be SSR'd, set `immediatelyRender` to `false` to avoid hydration mismatches.
       throw new Error(
         'Tiptap Error: SSR has been detected, and `immediatelyRender` has been set to `true` this is an unsupported configuration that may result in errors, explicitly set `immediatelyRender` to `false` to avoid hydration mismatches.',
       )
@@ -127,29 +118,30 @@ class EditorInstanceManager {
     return null
   }
 
-  /**
-   * Create a new editor instance. And attach event listeners.
-   */
-  private createEditor(): Editor {
-    const optionsToApply: Partial<EditorOptions> = {
-      ...this.options.current,
-      // Always call the most recent version of the callback function by default
-      onBeforeCreate: (...args) => this.options.current.onBeforeCreate?.(...args),
-      onBlur: (...args) => this.options.current.onBlur?.(...args),
-      onCreate: (...args) => this.options.current.onCreate?.(...args),
-      onDestroy: (...args) => this.options.current.onDestroy?.(...args),
-      onFocus: (...args) => this.options.current.onFocus?.(...args),
-      onSelectionUpdate: (...args) => this.options.current.onSelectionUpdate?.(...args),
-      onTransaction: (...args) => this.options.current.onTransaction?.(...args),
-      onUpdate: (...args) => this.options.current.onUpdate?.(...args),
-      onContentError: (...args) => this.options.current.onContentError?.(...args),
-      onDrop: (...args) => this.options.current.onDrop?.(...args),
-      onPaste: (...args) => this.options.current.onPaste?.(...args),
-      onDelete: (...args) => this.options.current.onDelete?.(...args),
+  private getWrappedOptions(additionalOptions?: Partial<EditorOptions>): Partial<EditorOptions> {
+    const currentOptions = this.options.current || {}
+    
+    return {
+      ...currentOptions,
+      ...additionalOptions,
+      onBeforeCreate: (...args) => this.options.current?.onBeforeCreate?.(...args),
+      onBlur: (...args) => this.options.current?.onBlur?.(...args),
+      onCreate: (...args) => this.options.current?.onCreate?.(...args),
+      onDestroy: (...args) => this.options.current?.onDestroy?.(...args),
+      onFocus: (...args) => this.options.current?.onFocus?.(...args),
+      onSelectionUpdate: (...args) => this.options.current?.onSelectionUpdate?.(...args),
+      onTransaction: (...args) => this.options.current?.onTransaction?.(...args),
+      onUpdate: (...args) => this.options.current?.onUpdate?.(...args),
+      onContentError: (...args) => this.options.current?.onContentError?.(...args),
+      onDrop: (...args) => this.options.current?.onDrop?.(...args),
+      onPaste: (...args) => this.options.current?.onPaste?.(...args),
+      onDelete: (...args) => this.options.current?.onDelete?.(...args),
     }
-    const editor = new Editor(optionsToApply)
+  }
 
-    // no need to keep track of the event listeners, they will be removed when the editor is destroyed
+  private createEditor(): Editor {
+    const optionsToApply = this.getWrappedOptions()
+    const editor = new Editor(optionsToApply)
 
     return editor
   }
@@ -196,11 +188,9 @@ class EditorInstanceManager {
           'onPaste',
         ].includes(key)
       ) {
-        // we don't want to compare callbacks, they are always different and only registered once
         return true
       }
 
-      // We often encourage putting extensions inlined in the options object, so we will do a slightly deeper comparison here
       if (key === 'extensions' && a.extensions && b.extensions) {
         if (a.extensions.length !== b.extensions.length) {
           return false
@@ -213,7 +203,6 @@ class EditorInstanceManager {
         })
       }
       if (a[key] !== b[key]) {
-        // if any of the options have changed, we should update the editor options
         return false
       }
       return true
@@ -226,28 +215,17 @@ class EditorInstanceManager {
    * @returns A cleanup function
    */
   onRender(deps: DependencyList) {
-    // The returned callback will run on each render
     return () => {
       this.isComponentMounted = true
-      // Cleanup any scheduled destructions, since we are currently rendering
       clearTimeout(this.scheduledDestructionTimeout)
 
       if (this.editor && !this.editor.isDestroyed && deps.length === 0) {
-        // if the editor does exist & deps are empty, we don't need to re-initialize the editor generally
         if (!EditorInstanceManager.compareOptions(this.options.current, this.editor.options)) {
-          // But, the options are different, so we need to update the editor options
-          // Still, this is faster than re-creating the editor
-          this.editor.setOptions({
-            ...this.options.current,
+          this.editor.setOptions(this.getWrappedOptions({
             editable: this.editor.isEditable,
-          })
+          }))
         }
       } else {
-        // When the editor:
-        // - does not yet exist
-        // - is destroyed
-        // - the deps array changes
-        // We need to destroy the editor instance and re-initialize it
         this.refreshEditorInstance(deps)
       }
 
@@ -263,9 +241,7 @@ class EditorInstanceManager {
    */
   private refreshEditorInstance(deps: DependencyList) {
     if (this.editor && !this.editor.isDestroyed) {
-      // Editor instance already exists
       if (this.previousDeps === null) {
-        // If lastDeps has not yet been initialized, reuse the current editor instance
         this.previousDeps = deps
         return
       }
@@ -273,19 +249,15 @@ class EditorInstanceManager {
         this.previousDeps.length === deps.length && this.previousDeps.every((dep, index) => dep === deps[index])
 
       if (depsAreEqual) {
-        // deps exist and are equal, no need to recreate
         return
       }
     }
 
     if (this.editor && !this.editor.isDestroyed) {
-      // Destroy the editor instance if it exists
       this.editor.destroy()
     }
 
     this.setEditor(this.createEditor())
-
-    // Update the lastDeps to the current deps
     this.previousDeps = deps
   }
 
@@ -298,13 +270,10 @@ class EditorInstanceManager {
     const currentInstanceId = this.instanceId
     const currentEditor = this.editor
 
-    // Wait two ticks to see if the component is still mounted
     this.scheduledDestructionTimeout = setTimeout(() => {
       if (this.isComponentMounted && this.instanceId === currentInstanceId) {
-        // If still mounted on the following tick, with the same instanceId, do not destroy the editor
         if (currentEditor) {
-          // just re-apply options as they might have changed
-          currentEditor.setOptions(this.options.current)
+          currentEditor.setOptions(this.getWrappedOptions())
         }
         return
       }
@@ -314,8 +283,6 @@ class EditorInstanceManager {
           this.setEditor(null)
         }
       }
-      // This allows the effect to run again between ticks
-      // which may save us from having to re-create the editor
     }, 1)
   }
 }
@@ -356,21 +323,16 @@ export function useEditor(options: UseEditorOptions = {}, deps: DependencyList =
 
   useDebugValue(editor)
 
-  // This effect will handle creating/updating the editor instance
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(instanceManager.onRender(deps))
 
-  // The default behavior is to re-render on each transaction
-  // This is legacy behavior that will be removed in future versions
   useEditorState({
     editor,
     selector: ({ transactionNumber }) => {
       if (options.shouldRerenderOnTransaction === false || options.shouldRerenderOnTransaction === undefined) {
-        // This will prevent the editor from re-rendering on each transaction
         return null
       }
 
-      // This will avoid re-rendering on the first transaction when `immediatelyRender` is set to `true`
       if (options.immediatelyRender && transactionNumber === 0) {
         return 0
       }
