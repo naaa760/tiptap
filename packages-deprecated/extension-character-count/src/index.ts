7e4b730c74aa87b17e58903c06311228e9968eb6
naaa760 <neh6a683@gmail.com>
2026-02-17 17:14:33 +0530

    fix: add Storage type augmentation for characterCount in deprecated package

--- packages-deprecated/extension-character-count/src/index.ts
+++ packages-deprecated/extension-character-count/src/index.ts
@@ -1,6 +1,13 @@
 import { CharacterCount } from '@tiptap/extensions'
+import type { CharacterCountStorage } from '@tiptap/extensions'
 
 export type { CharacterCountOptions } from '@tiptap/extensions'
 export { CharacterCount } from '@tiptap/extensions'
+
+declare module '@tiptap/core' {
+  interface Storage {
+    characterCount: CharacterCountStorage
+  }
+}
 
 export default CharacterCount
